f726cacac8f1c376fcb3f426913c64b4cd3e7f12
rsnakdmx <rsnakdmx@hotmail.com>
2025-06-25 02:04:36 -0600

    Fix vitest require startup error

--- test/fixtures/special-characters/index.js
+++ test/fixtures/special-characters/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import "./index.css";
 
 document.body.classList.add("@home", "+rounded", "button", ".md:w-1/3");
--- test/fixtures/unused/index.js
+++ test/fixtures/unused/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import "./index.css";
 
 document.body.classList.add(".used-class");
